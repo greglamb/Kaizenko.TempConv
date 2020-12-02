742567f4c34ae8e32b0d47f0b64f63c6eca98680
Greg Lamb <glamb@launchcg.com>
2020-12-02 12:14:02 -0800

    started vending machine

---
+++ Kaizenko.TempConv.Tests/VendingMachineTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kaizenko.TempConv;
+
+namespace Kaizenko.TempConv.Tests
+{
+    class VendingMachineTests
+    {
+        [Test]
+        public void ReleaseChange_WhenNoMoneyInserted_Expect0()
+        {
+            VendingMachine vendingMachine = new VendingMachine();
+            double change = vendingMachine.ReleaseChange();
+            Assert.AreEqual(0, change);
+        }
+
+
+
+    }
+}
---
+++ Kaizenko.TempConv/VendingMachine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaizenko.TempConv
+{
+    public class VendingMachine
+    {
+        public double ReleaseChange()
+        {
+            return 0;
+        }
+    }
+}

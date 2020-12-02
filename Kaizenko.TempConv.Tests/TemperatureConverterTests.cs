d8a122e18fd20bf5041ae86d585eeadbcaba4b08
Greg Lamb <glamb@launchcg.com>
2020-12-02 10:17:47 -0800

    tests working without implementation

---
+++ Kaizenko.TempConv.Tests/TemperatureConverterTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kaizenko.TempConv;
+
+namespace Kaizenko.TempConv.Tests
+{
+    public class TemperatureConverterTests
+    {
+        [Test]
+        public void ConverterCtoF_When0C_Expect23F()
+        {
+            var tempConveter = new TemperatureConveter();
+            var tempInF = tempConveter.ConvertCtoF(0);
+            Assert.AreEqual(32, tempInF);
+        }
+
+        [Test]
+        public void ConvertCtoF_When100C_Expect212F()
+        {
+            var tempConveter = new TemperatureConveter();
+            var tempInF = tempConveter.ConvertCtoF(100);
+            Assert.AreEqual(212, tempInF);
+        }
+
+        [Test]
+        public void ConvertCtoF_WhenNegative40C_ExpectNegative40F()
+        {
+            var tempConveter = new TemperatureConveter();
+            var tempInF = tempConveter.ConvertCtoF(-40);
+            Assert.AreEqual(-40, tempInF);
+        }
+
+        [Test]
+        public void ConvertCtoF_When37C_Expect98Point6F()
+        {
+            var tempConveter = new TemperatureConveter();
+            var tempInF = tempConveter.ConvertCtoF(37);
+            Assert.AreEqual(98.6, tempInF);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Kaizenko.TempConv/TemperatureConveter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaizenko.TempConv
+{
+    public class TemperatureConveter
+    {
+        public double ConvertCtoF(double tempInc)
+        {
+            switch (tempInc)
+            {
+                case 0: return 32;
+                case 100: return 212;
+                case -40: return -40;
+                case 37: return 98.6;
+            }
+            throw new InvalidOperationException("invalid op for argument: " + tempInc);
+        }
+    }
+}

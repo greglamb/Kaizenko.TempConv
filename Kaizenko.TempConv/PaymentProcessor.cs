343690390e610cd5f007bd03bb3903b7972443e0
Greg Lamb <glamb@launchcg.com>
2020-12-03 07:54:47 -0800

    PaymentProcessor scaffholding

---
+++ Kaizenko.TempConv/PaymentProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaizenko.TempConv
+{
+    public class PaymentProcessor
+    {
+        public void MakePayment(double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPaymentMade(double price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double ReturnChange()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

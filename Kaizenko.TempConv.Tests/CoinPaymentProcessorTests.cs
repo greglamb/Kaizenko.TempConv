31b2bc6239508102a9ead419298f87320999aa0e
Greg Lamb <glamb@launchcg.com>
2020-12-03 09:17:10 -0800

    adding tests for later

---
+++ Kaizenko.TempConv.Tests/CoinPaymentProcessorTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kaizenko.TempConv;
+
+namespace Kaizenko.TempConv.Tests
+{
+    class CoinPaymentProcessorTests
+    {
+        IPaymentProcessor paymentProcessor;
+
+        [SetUp]
+        public void Setup()
+        {
+            paymentProcessor = new CoinPaymentProcessor();
+        }
+
+        //[Test]
+        public void Something()
+        {
+            // arrange
+
+            // act
+
+            // asset
+            //Assert.AreEqual(0, );
+        }
+    }
+}
